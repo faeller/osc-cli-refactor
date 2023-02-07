e4f22612b996577702fdeefb08b98940ad38fa5e
Laurin Fäller <laurin.faeller@suse.com>
2023-02-07 13:44:32 +0100

    add prototype of command compatability layer

--- main.py
+++ main.py
@@ -5,12 +5,19 @@
 import os
 
 import osc.commandline_new as commandline
+import osc.commandline_compat_layer as compatibility_layer
 
 
 def main():
+    compatibility_layer.test()
     main_command = commandline.OscMainCommand()
     topdir = os.path.dirname(__file__)
     main_command.load_commands()
+    
+    for legacy_command in compatibility_layer.get_all_compat_wrapped_commands():
+        if not legacy_command.name in main_command.commands:
+            main_command.register(legacy_command)
+
     main_command.enable_autocomplete()
     args = main_command.parser.parse_args()
     main_command.execute(args)
---
+++ osc/commandline_compat_layer.py
@@ -0,0 +1,61 @@
+from osc.commandline_new import Command
+from osc.commandline import Osc
+import inspect
+
+
+def call_osc_babysitter_if_compat_failed():
+    pass
+
+
+def convert_osc_do_function_to_command_class(func):
+    class CompatabilityWrappedCommand(Command):
+        _func = func
+        name = _func.__name__[3:]
+
+        def __repr__(self):
+            result = super().__repr__()
+            result += f"({self._func.__name__})"
+            return result
+
+        def get_description(self):
+            result = self._func.__doc__ or ""
+            result = result.strip()
+            return result
+
+        def add_parser_arguments(self):
+            options = getattr(self._func, "options", [])
+            for option_args, option_kwargs in options:
+                self.parser.add_argument(*option_args, **option_kwargs)
+
+        def run(self, *args):
+            options = getattr(self._func, "options", [])
+            self._func(args, options)
+
+    return CompatabilityWrappedCommand
+
+
+def get_all_compat_wrapped_commands():
+    commands = []
+    for name in dir(Osc):
+        if not name.startswith("do_"):
+            continue
+
+        func = getattr(Osc, name)
+
+        if not inspect.isfunction(func):
+            continue
+
+        commands.append(convert_osc_do_function_to_command_class(func))
+        
+        # debug
+
+        # if name == "do_version":
+        #     return commands
+
+        
+    return commands
+
+
+def test():
+    # print(get_all_compat_wrapped_commands())
+    pass
